f49053e04e58cac483ac69eb5edcfe4550ec3ad5
Tlevi308 <64890144+Tlevi308@users.noreply.github.com>
2021-02-07 23:02:02 +0200

    main file

---
+++ Source.cpp
@@ -0,0 +1,16 @@
+
+#include "Lion.h"
+#include "Tiger.h"
+#include "Horse.h"
+#include "Butterfly.h"
+#include "Zoo.h"
+
+int Animal::_counterForSerial = 1000;
+
+int main() {
+
+	Zoo myZoo("Haim_kef");
+	myZoo.MENU();
+
+	return 0;
+}
